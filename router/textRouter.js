758a1ee09a50ac8f520e2d2c356a8a4300a6383b
Ramos Luis <ramosviana.luis@outlook.fr>
2022-07-25 21:56:19 +0200

    Veuillez trouver ici le test technique

--- router/textRouter.js
+++ router/textRouter.js
@@ -4,19 +4,73 @@ const router = express.Router();
 
 
 // c'est ici qu'il faut developper l'API se referer à la doc 
+/**
+ * 
+ * @param texte Chaine de caracteres.
+ * @returns Un tableau trié qui contient le nombre d'apparitions de chaque caractere.
+ */
+function compte_caracteres(texte){
+    let caracteres = {};
+    caracteres["total"] = texte.length;
+    for(let i = 0; i < caracteres["total"]; i++){
+        let lettre = texte[i];
+        if(lettre == " "){
+            lettre = "space";
+        }
+        if(caracteres[lettre]){
+            caracteres[lettre]++;
+        }
+        else{
+            caracteres[lettre] = 1;
+        }
+    }
+    let trie = Object.keys(caracteres).sort();
+    let newArray = {};
+    if (caracteres["total"]) {
+        newArray["total"] = caracteres["total"];
+    }
+    for (let i = 0; i < trie.length; i++) {
+        if (trie[i] != "total" && trie[i] != "space") {
+            newArray[trie[i]] = caracteres[trie[i]];
+        }
+    }
+    if (caracteres["space"]) {
+        newArray["space"] = caracteres["space"];
+    }
 
-router.get("/", (req, res, next) => {
-    //si tu envoies des données dans les requetes elles se trouveront dans req.query ou req.body selon la requete effectué "query" pour GET,  "body" pour POST
-    //req represente la requete
-    //res la reponse (voir les methodes dans la doc. La methode que tu vas utiliser vas etre "send" comme dans l'exemple ci dessous)
-    // next c'est pour enchainé les fonction comme celle ci les une à la suite des autres.
-    const payload = req.query || req.body;
-    console.log(payload);
+    return newArray;
+}
 
+/**
+ * 
+ * @param texte Chaine de caracteres.
+ * @returns Total de phrases.
+ */
+function compte_phrases(texte) {
+    let ponctuation = [".", "!", "?"];
+    let total = 0;
 
-    res.send({ message: "Bonjour Luis" })
+    if (texte.length > 0) {
+        total++;
+    }
+    for (let i = 0; i < texte.length; i++) {
+        if (ponctuation.includes(texte[i])) {
+            total++;
+        }
+    }
+    return {"total": total};
+}
+
+router.get("/characters/count", (req,res) => {
+    
+    let texte = req.body.text || req.query.text;
+    res.status(200).send(JSON.stringify(compte_caracteres(texte)));
 });
-router.get(/*Url de l'api  , Function */);
+
+router.get("/sentences/count", (req,res) => {
+    let texte = req.body.text || req.query.text;
+    res.status(200).send(JSON.stringify(compte_phrases(texte)));
+});
 
 
 module.exports = router;
