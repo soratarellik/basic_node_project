1b7d98f365eb01150e8b130f53633f19211fbdf7
Grégory Lucry <g.lucry@quos.fr>
2022-07-25 17:40:19 +0200

    aide au demmarage

--- router/textRouter.js
+++ router/textRouter.js
@@ -5,6 +5,17 @@ const router = express.Router();
 
 // c'est ici qu'il faut developper l'API se referer à la doc 
 
+router.get("/", (req, res, next) => {
+    //si tu envoies des données dans les requetes elles se trouveront dans req.query ou req.body selon la requete effectué "query" pour GET,  "body" pour POST
+    //req represente la requete
+    //res la reponse (voir les methodes dans la doc. La methode que tu vas utiliser vas etre "send" comme dans l'exemple ci dessous)
+    // next c'est pour enchainé les fonction comme celle ci les une à la suite des autres.
+    const payload = req.query || req.body;
+    console.log(payload);
+
+
+    res.send({ message: "Bonjour Luis" })
+});
 router.get(/*Url de l'api  , Function */);
 
 
